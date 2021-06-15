fd6b14d65e81dc319ce55a91f8a79b358fcf491d
Aleksandra Josko <olajosko41@gmail.com>
2021-06-15 12:20:12 +0200

    code formatting

--- src/Form/Input/index.js
+++ src/Form/Input/index.js
@@ -1,16 +1,19 @@
 import './index.css';
 
 const Input = ({ amount, setAmount }) => (
-    <input
-        value={amount}
-        className="form__field"
-        type="number" step="0.01"
-        min="0.01"
-        placeholder="Wpisz kwotę"
-        required
-        autoFocus
-        onChange={({ target }) => setAmount(target.value)}
-    />
+    <p>
+        <input
+            value={amount}
+            className="input"
+            type="number" 
+            step="0.01"
+            min="0.01"
+            placeholder="Wpisz kwotę"
+            required
+            autoFocus
+            onChange={({ target }) => setAmount(target.value)}
+        />
+    </p>
 );
 
 export default Input;
