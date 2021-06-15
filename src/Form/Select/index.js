6cbfc054c46cbadcccdb1392c11ddac03f13d5a4
Aleksandra Josko <olajosko41@gmail.com>
2021-06-15 13:00:52 +0200

    className changed and code formatting

--- src/Form/Select/index.js
+++ src/Form/Select/index.js
@@ -3,13 +3,12 @@ import './index.css';
 const Select = ({ currencies, currency, setCurrency }) => (
 
     <select
-        className="form__field"
+        className="select"
         required
         onChange={({ target }) => setCurrency(target.value)}
         value={currency}
     >
-        {
-        currencies.map(currency => (
+        {currencies.map(currency => (
             <option key={currency.id} value={currency.id}>
                 {currency.name}
             </option>
