ac67b2b352cea96bbf65fbdaf3ffbbd3510c050f
Aleksandra Josko <olajosko41@gmail.com>
2021-06-15 12:59:11 +0200

    paragraphs in Input component removed

--- src/Form/Input/index.js
+++ src/Form/Input/index.js
@@ -1,19 +1,17 @@
 import './index.css';
 
 const Input = ({ amount, setAmount }) => (
-    <p>
-        <input
-            value={amount}
-            className="input"
-            type="number" 
-            step="0.01"
-            min="0.01"
-            placeholder="Wpisz kwotę"
-            required
-            autoFocus
-            onChange={({ target }) => setAmount(target.value)}
-        />
-    </p>
+    <input
+        value={amount}
+        className="input"
+        type="number"
+        step="0.01"
+        min="0.01"
+        placeholder="Wpisz kwotę"
+        required
+        autoFocus
+        onChange={({ target }) => setAmount(target.value)}
+    />
 );
 
 export default Input;
