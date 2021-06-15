c9d8d355c94ae15b1ec5dacee09310dea634e367
Aleksandra Josko <olajosko41@gmail.com>
2021-06-15 13:27:16 +0200

    curriencies moved to separte file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import Form from './Form';
-
-const currencies = [
-  { id: "EUR", exchangeRate: 4.5829, name: "Euro", },
-  { id: "GBP", exchangeRate: 5.2908, name: "Funt brytyjski", },
-  { id: "USD", exchangeRate: 3.8066, name: "Dolar amerykański", },
-];
+import currencies from './curriencies.js';
 
 function App() {
   return (
---
+++ src/curriencies.js
@@ -0,0 +1,7 @@
+const currencies = [
+    { id: "EUR", exchangeRate: 4.5829, name: "Euro", },
+    { id: "GBP", exchangeRate: 5.2908, name: "Funt brytyjski", },
+    { id: "USD", exchangeRate: 3.8066, name: "Dolar amerykański", },
+  ];
+
+  export default currencies;
