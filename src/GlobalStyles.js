5a8ed856fe4b52f764e4f9d4a9e2607ec49000ba
Aleksandra Josko <olajosko41@gmail.com>
2021-06-30 13:22:58 +0200

    background added

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
+import background from './background.jpeg';
 
 
 export const GlobalStyle = createGlobalStyle`
@@ -19,7 +20,7 @@ export const GlobalStyle = createGlobalStyle`
     padding: 10px;
     font-family: 'Barlow Condensed', sans-serif;
     font-size: 22px;
-    background-image: url("https://image.freepik.com/darmowe-wektory/analiza-rynku-akcji-i-handel-akcjami-symbole-walut-wykresy-biznesowe-i-globalne-transfery-pieniezne_115579-1283.jpg");
+    background-image: url("${background}");
     background-size: cover;
   }
 `;
